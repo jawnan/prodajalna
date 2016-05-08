7363657d90fda0abeb25301c5db4f96043793dbe
jawnan <jawnan@github.com>
2016-05-08 22:04:38 +0200

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -208,22 +208,27 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Address, City, State, Country, PostalCode, \
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-      //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State, polja.Country,
+          polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
-  
+    if(napaka2)message = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in" +
+        " poskusite znova";
+    else message = "Stranka je bila uspešno registrirana";
+    odgovor.redirect("#");
     odgovor.end();
   });
-})
-
+});
+message = "";
 // Prikaz strani za prijavo
 streznik.get('/prijava', function(zahteva, odgovor) {
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
-        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+        odgovor.render('prijava', {sporocilo: message, seznamStrank: stranke, seznamRacunov: racuni});
+        message = "";
       }) 
     });
 })
